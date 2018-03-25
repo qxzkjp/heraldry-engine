f0fb3968b0ad9345d4ad6f06ecccb52b5fa415df
Deus Kane <github.qxzkjp@spamgourmet.com>
2018-03-25 19:42:32 +0100

    Default record for users
    Added an SQL command to insert a user called "admin" with password
    "password". To be used on local systems only.

--- SQL/users.sql
+++ SQL/users.sql
@@ -7,3 +7,7 @@ CREATE TABLE IF NOT EXISTS `Users` (
   accessLevel int(11) NOT NULL,
   PRIMARY KEY (ID)
 );
+
+/*this creates a record for admin with password "password"*/
+INSERT INTO USERS (userName, pHash, accessLevel)
+VAULES ("admin", "$2y$10$0ZjS.jQJUWgiowJKwZFrpubNtliB0TEup7beIhiuNEL7l0WxNxMZK", 0);
